fcc1fba1f844e06fb5918522f9a1571ce33293cb
Max Sargent <maxsargentdev@gmail.com>
2020-11-22 20:02:19 +0000

    feat: added participant generation

--- generate.py
+++ generate.py
@@ -10,15 +10,45 @@ directory but can be modified.
 import os
 import sys
 import argparse
-
-def generate_tree(directory):
-    print(os.listdir(directory))
+import re
+import json
 
 def dir_path(string):
     if os.path.isdir(string):
         return string
     else:
         raise NotADirectoryError(string)
+
+def sorted_alphanumeric(data):
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
+    return sorted(data, key=alphanum_key)
+
+def generate_participants(directory):
+    json_files = sorted_alphanumeric([pos_json for pos_json in os.listdir(directory) if pos_json.endswith('.json')])
+    json_files.reverse()
+
+    participants_list = []
+
+    for index, js in enumerate(json_files):
+        with open(os.path.join(directory, js)) as json_file:
+            json_text = json.load(json_file)
+
+            messages = json_text['messages']
+
+            for message in messages:
+                participants_list.append(message['sender_name'])
+    
+    participants_list = sorted(list(dict.fromkeys(participants_list)))
+    
+    return participants_list
+
+def generate_graph(directory):
+    participants = generate_participants(directory)
+
+    for participant in participants:
+        nickname_dict = {}
+        print(participant)
 
 def main(arguments):
 
@@ -31,7 +61,7 @@ def main(arguments):
 
     args = parser.parse_args(arguments)
 
-    generate_tree(args.indirectory)
+    generate_graph(args.indirectory)
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
